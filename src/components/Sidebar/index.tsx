168a15ec94007989f65019a7d5cafbab7463345c
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2024-04-10 11:43:55 -0300

    init

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,11 @@
+import { Logo } from "./Logo";
+
+export function SideBar() {
+  return (
+    <aside className="border-r border-zinc-200 px-5 py-8">
+      <Logo />
+      <div>
+      </div>
+    </aside>
+  )
+}
